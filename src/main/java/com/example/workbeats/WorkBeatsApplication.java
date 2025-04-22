1786c22b020b33136825e0c637985fb0696765da
Regina ... <regina.hernandezrdr@uanl.edu.mx>
2025-04-21 19:48:27 -0600

    primer commit

---
+++ src/main/java/com/example/workbeats/WorkBeatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkBeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workbeats/WorkBeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workbeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
